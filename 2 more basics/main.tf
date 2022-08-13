9a48a5ebe8cff7054ab4ed80dd1d970b2e083d28
vasu8480 <vasu8480@gmail.com>
2022-08-13 11:13:14 +0530

    new file:   .cph/.psadf.py_60eadb76ca1467101463566ec3dddc17.prob 	modified:   2 more basics/main.tf 	new file:   2 more basics/terraform.tfvars

--- 2 more basics/main.tf
+++ 2 more basics/main.tf
@@ -1,8 +1,16 @@
-provider "aws" {
-	region = "us-east-1"
+variable "iam_user_name_prefix" {
+  type    = string #any, number, bool, list, map, set, object, tuple
+  default = "my_iam_user"
 }
 
-resource "aws_iam_user" "creating_users" {
-	count=3	// if count increses, then the extra user will be created
-	name = "terraform_user_vasu${count.index}"		
+provider "aws" {
+  region = "us-east-1"
 }
+
+resource "aws_iam_user" "my_iam_users" {
+  count = 1
+  name  = "${var.iam_user_name_prefix}_${count.index}"
+}
+
+// you can pass the value from command line to the variable or tfvars
+// terraform apply -refresh=flase -var="iam_user_name_prefix=value from command line"
