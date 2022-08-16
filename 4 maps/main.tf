5841f1dc2a4adc92e48e5135541b24a86e7bdd4c
vasu8480 <vasu8480@gmail.com>
2022-08-16 10:46:29 +0530

    maps

--- 4 maps/main.tf
+++ 4 maps/main.tf
@@ -1,28 +1,28 @@
-variable "users"{
-# default = {
-# 						sachin:"india",
-# 						Vasu:"Ap",
-# 						ravi:"kakinada",
-# 						rohit:"mumbai"
-# 					}
-default = {
-						sachin:{country:"india", departement:"abc"},
-						Vasu:{country:"Ap", departement:"xyz"},
-						ravi:{country:"kakinada ", departement:"pqr"}
-					}	
+variable "users" {
+  # default = {
+  # 						sachin:"india",
+  # 						Vasu:"Ap",
+  # 						ravi:"kakinada",
+  # 						rohit:"mumbai"
+  # 					}
+  default = {
+    sachin : { country : "india", departement : "abc" },
+    Vasu : { country : "Ap", departement : "xyz" },
+    ravi : { country : "kakinada ", departement : "pqr" }
+  }
 }
 provider "aws" {
-	region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_iam_user" "my_iam_user_creating" {
-	for_each = var.users
-	name=each.key
-	# tags={
-	# 	country:each.value
-	# }
-	tags={
-		country:each.value.country
-		departement:each.value.departement
-	}
+  for_each = var.users
+  name     = each.key
+  # tags={
+  # 	country:each.value
+  # }
+  tags = {
+    country : each.value.country
+    departement : each.value.departement
+  }
 }
