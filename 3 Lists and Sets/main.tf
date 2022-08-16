abcb1690cf5050fa5edbc8e4c2751555120c1a45
vasu8480 <vasu8480@gmail.com>
2022-08-16 10:30:48 +0530

    lists

--- 3 Lists and Sets/main.tf
+++ 3 Lists and Sets/main.tf
@@ -1,11 +1,14 @@
 variable "names"{
-	default = [ "Vasu","ravi","rohit"]
+	default = ["sachin","Vasu","ravi","rohit"]
+	#default = [ "Vasu","ravi","rohit"]
 }
 provider "aws" {
 	region = "us-east-1"
 }
 
 resource "aws_iam_user" "my_iam_user_creating" {
-	count=length(var.names)
-	name = var.names[count.index]
+	#count=length(var.names)       // this doesnt work properly add user before the lists and middle of the list
+	#name = var.names[count.index]
+	for_each = toset(var.names)
+	name = each.value
 }
