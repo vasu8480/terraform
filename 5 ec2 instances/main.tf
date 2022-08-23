2e40e34b7c6df2a2bb4fc8def08f3a49765b2cbf
vasu8480 <vasu8480@gmail.com>
2022-08-23 12:44:11 +0530

    ec2 update

--- 5 ec2 instances/main.tf
+++ 5 ec2 instances/main.tf
@@ -7,11 +7,35 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_default_vpc" "default" {
+
+}
+
+data "aws_subnets" "default_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [aws_default_vpc.default.id]
+  }
+}
+
+data "aws_ami" "aws_linux_2_latest" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*"]
+  }
+}
+
+data "aws_ami_ids" "aws_linux_2_latest_ids" {
+  owners = ["amazon"]
+}
+
 //security group
 resource "aws_security_group" "http_server_sg" {
   name = "http_server_sg"
-  //vpc_id = "vpc-c49ff1be"
-  vpc_id = "vpc-f73bab8a"
+  //vpc_id = "vpc-f73bab8a" -- if you want to use the vpc
+  vpc_id = aws_default_vpc.default.id
 
   ingress {
     from_port   = 80
@@ -40,11 +64,13 @@ resource "aws_security_group" "http_server_sg" {
 }
 
 resource "aws_instance" "http_server" {
-  ami                    = "ami-090fa75af13c156b4"
+  //ami                    = "ami-090fa75af13c156b4"
+  ami                    = data.aws_ami.aws_linux_2_latest.id
   key_name               = "aws-key-pair"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.http_server_sg.id]
-  subnet_id              = "subnet-53d79335"
+  //subnet_id              = "subnet-53d79335"
+  subnet_id = tolist(data.aws_subnets.default_subnets.ids)[0]
   tags = {
     Name = "http_server"
   }
